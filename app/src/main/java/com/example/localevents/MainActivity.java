4ece1eb8a7c9834cfeda54db2ffd0c2ac7769fca
sukhbirsekhon <sukhbirsekhon3939@gmail.com>
2020-02-25 17:05:58 -0500

    Started the android project

---
+++ app/src/main/java/com/example/localevents/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.localevents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
